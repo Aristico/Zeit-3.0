43071a72adaa06a7118793ad988314c45beb9cc4
Christian <christianohning@ChristiansMBP2.fritz.box>
2018-11-18 14:20:41 +0100

    Installation, User Anlegen und Bearbeiten fertig

---
+++ resources/views/layouts/time.blade.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html lang="de">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="/css/all.css">
+
+    <title>Hello, world!</title>
+  </head>
+  <body>
+        <div class="container">
+
+            @yield('content')
+
+        </div>
+    <script src="/js/all.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+  </body>
+</html>
